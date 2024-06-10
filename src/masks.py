7f46058ccbb41a8d5b1585b0ae3d274941ce624b
Artem Kuzmin <frankiekoo72@mail.ru>
2024-06-10 17:42:22 +0300

    коммит инициализации

---
+++ src/masks.py
@@ -0,0 +1,17 @@
+def get_mask_card_number(card: str) -> str | None:
+    """Функция маскировки номера карты"""
+    if card.isdigit() and len(card) == 16:
+        return f"{card[:4]} {card[4:6]} ** **** {card[12:16]}"
+    else:
+        return None
+
+
+def get_mask_account(acc: str) -> str | None:
+    """Функция маскировки номера счета"""
+    if acc.isdigit() and len(acc) == 20:
+        return f"**{acc[16:20]}"
+    else:
+        return None
+
+
+print(get_mask_card_number(input()))
