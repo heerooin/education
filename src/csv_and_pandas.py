6da467ffd3b525541aaf079d8744eacdb12a6377
Artem Kuzmin <frankiekoo72@mail.ru>
2025-03-31 12:30:49 +0300

    UPD

--- src/csv_and_pandas.py
+++ src/csv_and_pandas.py
@@ -3,13 +3,17 @@ import pandas as pd
 
 
 def csv_open(path: str) -> list[dict]:
+    csv_transactions = []
     with open(path) as file:
         reader = csv.reader(file, delimiter=';')
-        next(reader)
+        headers = next(reader)
         for row in reader:
-            print(row)
+            csv_transaction = dict(zip(headers, row))
+            csv_transactions.append(csv_transaction)
+        return csv_transactions
 
 
 def xlsx_open(path: str) -> list[dict]:
     excel_data = pd.read_excel(path)
-    return excel_data
+    xlsx_transactions = excel_data.to_dict()
+    return xlsx_transactions
